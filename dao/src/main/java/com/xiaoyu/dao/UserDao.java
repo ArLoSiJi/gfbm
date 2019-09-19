34fb664ff8d2897765f59e7a12f9f4b7a7227326
SiLiJion <826105546@qq.com>
2019-09-19 23:55:51 +0800

    Initial commit

---
+++ dao/src/main/java/com/xiaoyu/dao/UserDao.java
@@ -0,0 +1,7 @@
+package com.xiaoyu.dao;
+
+/**
+ * @author shkstart@create 2019-09-19 22:07
+ */
+public class UserDao {
+}
---
+++ dao/src/main/java/com/xiaoyu/dao/impl/UserDaoImpl.java
@@ -0,0 +1,7 @@
+package com.xiaoyu.dao.impl;
+
+/**
+ * @author shkstart@create 2019-09-19 22:08
+ */
+public class UserDaoImpl {
+}
